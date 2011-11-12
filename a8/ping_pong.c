0f823b2585d8329d6974b1b6d9f1fa733bc95930
Jonathan <ksk.logan@hotmail.com>
2011-11-11 23:20:23 -0800

    Finished a8.

--- a8/ping_pong.c
+++ a8/ping_pong.c
@@ -4,43 +4,74 @@
 
 #define NUM_ITERATIONS 1000
 
-/* Print out 'I' and wait for a while.  */
-void* 
-ping ( void* x ) 
+/* Struct holding a computation. */
+struct Triple
 {
-		int i,j;
-		for (i = 0; i < NUM_ITERATIONS; i++ ) 
-		{
-				printf( "I" );
-				for ( j = 0; j < 10000; j++ ) {}
-				uthread_yield();
-		}
+		int arg0;
+		int arg1;
+};
+
+/* Procedure for adding args of a Triple struct. */
+void *
+add ( void *ts )
+{
+		struct Triple *x = (struct Triple*) ts;
+		int *result = (int *) malloc(sizeof ( int ) ); 
+		*result = x->arg0 + x->arg1;
+		return (void *) result;
 }
 
-/* Print out 'O' and wait for a while.  */
-void* 
-pong ( void* x ) 
+/* Procedure for subtracting args of a Triple struct. */
+void *
+sub ( void *ts )
 {
-		int i,j;
-		for ( i = 0; i < NUM_ITERATIONS; i++ ) 
-		{
-				printf( "O" );
-				for ( j = 0; j < 10000; j++ ) {}
-				uthread_yield();
-		}
+		struct Triple *x = (struct Triple*) ts;
+		int *result = (int *) malloc( sizeof( int ) ); 
+		*result = x->arg0 - x->arg1;
+		return (void *) result;
 }
 
-/* Main.  */
+/* Main: Compute ((1 + 2) - (3 + 4)) + 7 with threads. */
 int 
 main ( int* argc, char** argv ) 
 {
-		uthread_t *ping_thread, *pong_thread;
-		int i;
-		uthread_init( 1 );
-		ping_thread = uthread_create( ping, 0 );
-		pong_thread = uthread_create( pong, 0 );
-		uthread_join( ping_thread );
-		uthread_join( pong_thread );
-		printf( "\n" );
+		uthread_init( 2 );
+
+		/* Create structs for (1 + 2) and (3 + 4). */
+		struct Triple *t1 = (struct Triple*) malloc( sizeof( struct Triple ) );
+		t1->arg0 = 1;
+		t1->arg1 = 2;
+		struct Triple *t2 = (struct Triple*) malloc( sizeof( struct Triple ) );
+		t2->arg0 = 3;
+		t2->arg1 = 4;
+
+		/* Create threads for (1 + 2) and (3 + 4). */
+		uthread_t *t1_thread, *t2_thread;
+		t1_thread = uthread_create( add, (void*) t1 );
+		t2_thread = uthread_create( add, (void*) t2 );
+
+		/* Execute (1 + 2) and (3 + 4) and store results. */
+		int r1 = *( (int *) uthread_join( t1_thread ) );
+		int r2 = *( (int *) uthread_join( t2_thread ) );
+
+		/* Create struct for (r1 - r2). */
+		struct Triple *t3 = (struct Triple*) malloc( sizeof( struct Triple ) );
+		t3->arg0 = r1;
+		t3->arg1 = r2;
+
+		/* Create thread for (r1 - r2). */
+		uthread_t *t3_thread = uthread_create ( sub, (void *) t3 );
+		int r3 = *( (int *) uthread_join( t3_thread ) );
+
+		/* Create struct for (r3 + 7). */
+		struct Triple *t4 = (struct Triple*) malloc( sizeof( struct Triple ) );
+		t4->arg0 = r3;
+		t4->arg1 = 7;
+
+		/* Create thread for (r3 + 7). */
+		uthread_t *t4_thread = uthread_create ( add, (void *) t4 );
+		int r4 = *( (int *) uthread_join( t4_thread ) );
+		
+		printf( "%d\n", r4 );
 }
 
